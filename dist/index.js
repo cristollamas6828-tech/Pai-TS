da636ba9a8e9119dfd5c938c57a7e15d46ec2f3e
cristollamas6828-tech <cristo.llamas6828@alumnos.udg.mx>
2026-01-30 20:04:05 -0600

    Actividad(30/01/2026)

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const suma = (a, b) => {
+    return a + b;
+};
+const resultado = suma(10, 3);
+console.log(`El resultado es:${resultado}`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const suma = (a:number, b: number): number => {
+    return a + b;
+}
+
+const resultado = suma (10,11)
+console.log(`El resultado es: ${resultado}`);
+/*
+
+*/
